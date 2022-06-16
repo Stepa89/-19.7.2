81108d55d5f10a6885f01914c63409b4aa5b1070
Stepa89 <101787971+Stepa89@users.noreply.github.com>
2022-06-16 16:23:56 +0300

    Create test_pet_friends.py

---
+++ test_pet_friends.py
@@ -0,0 +1,57 @@
+from api import PetFriends
+from settings import valid_email, valid_password
+import os
+
+
+pf = PetFriends()
+
+
+def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
+#Получение api ключа
+    status, result = pf.get_api_key(email, password)
+    assert status == 200
+    assert 'key' in result
+
+
+def test_get_all_pets_with_valid_key(filter=''):
+#Получение списка питомцев
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.get_list_of_pets(auth_key, filter)
+    assert status == 200
+    assert len(result['pets']) > 0
+
+
+def test_add_new_pet_with_valid_data(name ='Булка', animal_type = 'собака', age = '4',
+                                     pet_photo= 'images/dog1.jpg'):
+# Проверка возможности что можно добавить питомца с корректными данными
+    pet_photo = os.path.join(os.path.dirname(__file__), pet_photo)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)
+    assert status == 200
+    assert result['name'] == name
+
+
+def test_successful_update_self_pet_info(name='Лиза', animal_type='кошка', age=15):
+# Проверка возможности обновления информации о питомце
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    if len(my_pets['pets']) > 0:
+        status, result = pf.update_pet_info(auth_key, my_pets['pets'][0]['id'], name, animal_type, age)
+        assert status == 200
+        assert result['name'] == name
+    else:
+        raise Exception("There is no my pets")
+
+
+def test_successful_delete_self_pet():
+# Проверка метода удаления питомца
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet(auth_key, "Боня", "собака", "8", "images/dog1.jpg")
+        _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    pet_id = my_pets['pets'][0]['id']
+    status, _ = pf.delete_pet(auth_key, pet_id)
+    _, my_pets = pf.get_list_of_pets(auth_key, "my_pets")
+    assert status == 200
+    assert pet_id not in my_pets.values()
